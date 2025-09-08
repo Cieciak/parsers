17fd4cc53ba030377feb5c78ae2e9f6b6866d253
Cieciak <nikodemrzasa@wp.pl>
2025-09-09 00:09:33 +0200

    Add docstrings to Parser class

--- src/zjadacz/parser.py
+++ src/zjadacz/parser.py
@@ -4,29 +4,39 @@ from .status import Status
 from .error import ParserError
 
 class Parser:
+    '''Default parser class.\nImplements: `.map`, `.chain`, `.match` and `.reassign`'''
 
     def __init__(self, transformer: Callable[[Status], Status]):
-        self.transformer: Callable[[Status], Status] = transformer
+        self.__transformer: Callable[[Status], Status] = transformer
 
-    def run(self, initial: Status) -> Status:
-        return self.transformer(initial)
+    @property
+    def transformer(self) -> Callable[[Status], Status]:
+        '''READ ONLY. Return current parser transformer'''
+        return self.__transformer
+
+    def run(self, status: Status) -> Status:
+        '''Execute parser on `status` and return structured data'''
+        return self.__transformer(status)
 
     def map(self, function: Callable[[Status], Any]) -> Self:
+        '''Map parser result using `function`.\n`function` should return only data'''
         def wrapper(status: Status) -> Status:
-            current = self.transformer(status)
+            current = self.run(status)
             if isinstance(current, ParserError): return current
             return current.chainResult(function(current), increment=0)
         return Parser(wrapper)
 
     def chain(self, function: Callable[[Status], Self]) -> Self:
+        '''Chain next parser based on returned status'''
         def wrapper(status: Status) -> Status:
-            current = self.transformer(status)
+            current = self.run(status)
             if isinstance(current, ParserError): return current
             nextParser = function(current)
-            return nextParser.transformer(current)
+            return nextParser.run(current)
         return Parser(wrapper)    
     
-    def match(self, cases: dict) -> Self:
+    def match(self, cases: dict[Any, Self]) -> Self:
+        '''Select parser from `cases` based on last status'''
         def wrapper(status: Status) -> Status:
             current = self.run(status)
             if isinstance(current, ParserError): return current
@@ -36,4 +46,5 @@ class Parser:
         return Parser(wrapper)
         
     def reassign(self, parser: Self):
-        self.transformer = parser.transformer
+        '''Assign new transformer'''
+        self.__transformer = parser.transformer
